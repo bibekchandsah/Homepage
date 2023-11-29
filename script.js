ee8fd0d32a5a894fb1e9fe44e61d851c46493b9e
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2023-11-29 22:47:49 +0530

    side button for calendar
    side button for calendar to show and hide

--- script.js
+++ script.js
@@ -271,5 +271,31 @@ function loadIcons() {
 
 loadIcons();
 
+// side button for calendar website
+document.addEventListener("DOMContentLoaded", function () {
+    const colorSwitcher = document.getElementById("color-switcher");
+    const themeContainer = document.getElementById("theme-buttons-container");
+
+    colorSwitcher.addEventListener("click", function () {
+        // Check if the theme container is already populated
+        if (themeContainer.childElementCount === 0) {
+            // Create an iframe element
+            const iframe = document.createElement("iframe");
+            iframe.src = "https://bibek10550.github.io/calander/";
+            iframe.width = "453";
+            iframe.height = "693";
+            iframe.style.border = "none";
+
+            // Append the iframe to the theme container
+            themeContainer.appendChild(iframe);
+        }
+
+        // Toggle the active class on color-switcher for styling
+        colorSwitcher.classList.toggle("active");
+    });
+});
+
+
+
 
 
