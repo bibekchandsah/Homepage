2879c4efb0f205f0ae87c59219a3e9ceb28a5334
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2023-12-27 16:09:03 +0530

    now image ratio
    image is uploaded to 3mb

--- profileChange.js
+++ profileChange.js
@@ -26,22 +26,29 @@ file.addEventListener('change', function () {
     // This refers to the file input element
     const choosedFile = this.files[0];
     if (choosedFile) {
-        const reader = new FileReader(); // FileReader is a predefined function of JS
-        reader.addEventListener('load', function () {
-            // Resize the image before storing and displaying
-            resizeImage(reader.result, 250, 250, (resizedImage) => {
+        // Check if the file size is less than or equal to 3MB
+        if (choosedFile.size <= 3 * 1024 * 1024) { // 3MB in bytes
+            const reader = new FileReader(); // FileReader is a predefined function of JS
+            reader.addEventListener('load', function () {
+                img.setAttribute('src', reader.result);
+                localStorage.setItem('profileImage', reader.result);
+            });
+            reader.readAsDataURL(choosedFile);
+        } else {
+            // Resize the image before storing and displaying, limiting to 3MB
+            resizeImage(choosedFile, 250, 250, 3 * 1024 * 1024, (resizedImage) => {
                 img.setAttribute('src', resizedImage);
                 localStorage.setItem('profileImage', resizedImage);
             });
-        });
-        reader.readAsDataURL(choosedFile);
+        }
     }
 });
 
 // Resize the image using canvas
-function resizeImage(inputDataURL, maxWidth, maxHeight, callback) {
+function resizeImage(inputFile, maxWidth, maxHeight, maxSizeInBytes, callback) {
     const img = new Image();
-    img.src = inputDataURL;
+    img.src = URL.createObjectURL(inputFile);
+
     img.onload = function () {
         const canvas = document.createElement('canvas');
         let width = img.width;
@@ -63,12 +70,26 @@ function resizeImage(inputDataURL, maxWidth, maxHeight, callback) {
         const ctx = canvas.getContext('2d');
         ctx.drawImage(img, 0, 0, width, height);
 
-        // Increase the quality to 0.9 (90%)
-        const outputDataURL = canvas.toDataURL('image/jpeg', 0.99);
+         // Increase the quality to 0.9 (90%) 0-1
+        let outputDataURL = canvas.toDataURL('image/jpeg', 1);
+
+        // Check if the size is greater than maxSizeInBytes
+        while (outputDataURL.length > maxSizeInBytes && width > 10 && height > 10) {
+            // Reduce image dimensions and quality until it fits within maxSizeInBytes
+            width *= 0.9;
+            height *= 0.9;
+
+            canvas.width = width;
+            canvas.height = height;
+
+            ctx.drawImage(img, 0, 0, width, height);
+
+            outputDataURL = canvas.toDataURL('image/jpeg', 0.9);
+        }
+
         callback(outputDataURL);
     };
 }
-
 
 // clean the local storage
 // Add a click event listener to the button
