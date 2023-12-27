34dde8f8e75fbb3e8d837a7a155ef74923369aa0
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2023-12-28 03:22:28 +0530

    image prompt

--- script.js
+++ script.js
@@ -258,6 +258,32 @@ function setWhatsappLink() {
 // setWhatsappLink();
 
 
+// copy image prompt
+document.addEventListener('DOMContentLoaded', function () {
+    const promptCopyButtons = document.querySelectorAll('.promptCopy');
+
+    promptCopyButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            const cardText = this.parentElement.querySelector('.card-text');
+
+            navigator.clipboard.writeText(cardText.textContent).then(function () {
+                // Display "Copied!" message
+                button.innerHTML = 'Copied';
+                setTimeout(function () {
+                    button.innerHTML = 'Copy';
+                }, 3000);
+            }).catch(function (err) {
+                console.error('Unable to copy text to clipboard', err);
+                button.innerHTML = 'Error';
+                setTimeout(function () {
+                    button.innerHTML = 'Copy';
+                }, 3000);
+            });
+        });
+    });
+});
+
+
 // icon images stored locally
 // Data with website details
 const data = `
