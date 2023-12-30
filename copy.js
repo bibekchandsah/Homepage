5bb40084af6eb9693238eb7844a1b8d918781f87
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2023-12-30 12:17:48 +0530

    added filter search

--- copy.js
+++ copy.js
@@ -22,3 +22,45 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+// when click on search dropdown apperar
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+window.onload = function () {
+    // var SearchResult = document.getElementById("ResultBox");
+    var SearchResult = document.getElementById("ResultBox");
+    document.onclick = function (event) {
+        if (event.target.id !== "ResultBox") {
+            SearchResult.style.display = "block";
+            resultsBox.style.animation = "to-bottom 0.3s ease-in"
+        }
+        if (event.target.id !== "input-box") {
+            // SearchResult.style.display = "none";
+            // Hide the volume bar with an animation
+            resultsBox.style.animation = "to-top 0.3s ease-in";
+            setTimeout(() => {
+                SearchResult.style.display = "none";
+                resultsBox.style.animation = ""; // Reset animation property
+            }, 300); // Adjust the timeout to match the animation duration
+        }
+    }
+};
+
+
+
+// filter
+const search = () => {
+    let filter = document.getElementById('input-box').value.toUpperCase();
+    let ul = document.getElementById('ResultBox');
+    let li = ul.getElementsByTagName('li');
+    for (var i = 0; i < li.length; i++) {
+        let a = li[i].getElementsByTagName('a')[0];
+        let textValue = a.textContent || a.innerHTML;
+        if (textValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+}
