69ef374bb0a85f5e7743da9a60afb022f8b7b47c
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2023-12-30 15:47:35 +0530

    added custom search

--- copy.js
+++ copy.js
@@ -25,27 +25,84 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 // when click on search dropdown apperar
-const resultsBox = document.querySelector(".result-box");
-const inputBox = document.getElementById("input-box");
-window.onload = function () {
-    // var SearchResult = document.getElementById("ResultBox");
+// const resultsBox = document.querySelector(".result-box");
+// const inputBox = document.getElementById("input-box");
+// window.onload = function () {
+//     // var SearchResult = document.getElementById("ResultBox");
+//     var SearchResult = document.getElementById("ResultBox");
+//     document.onclick = function (event) {
+//         if (event.target.id == "input-box") {
+//             SearchResult.style.display = "block";
+//             resultsBox.style.animation = "to-bottom 0.3s ease-in"
+//         }
+//         if (event.target.id !== "input-box") {
+//             // SearchResult.style.display = "none";
+//             // Hide the volume bar with an animation
+//             resultsBox.style.animation = "to-top 0.3s ease-in";
+//             setTimeout(() => {
+//                 SearchResult.style.display = "none";
+//                 resultsBox.style.animation = ""; // Reset animation property
+//             }, 300); // Adjust the timeout to match the animation duration
+//         }
+//     }
+// };
+
+document.addEventListener("DOMContentLoaded", function () {
+    const resultsBox = document.getElementById("ResultBox");
+    const ResultsBox = document.querySelector(".result-box");
+    const inputBox = document.getElementById("input-box");
     var SearchResult = document.getElementById("ResultBox");
-    document.onclick = function (event) {
+
+    // Handle click on document to close the results box if clicked outside
+    document.addEventListener("click", function (event) {
         if (event.target.id == "input-box") {
             SearchResult.style.display = "block";
-            resultsBox.style.animation = "to-bottom 0.3s ease-in"
+            ResultsBox.style.animation = "to-bottom 0.3s ease-in"
         }
-        if (event.target.id !== "input-box") {
-            // SearchResult.style.display = "none";
-            // Hide the volume bar with an animation
-            resultsBox.style.animation = "to-top 0.3s ease-in";
+        if (!resultsBox.contains(event.target) && event.target !== inputBox) {
+            ResultsBox.style.animation = "to-top 0.3s ease-in";
             setTimeout(() => {
                 SearchResult.style.display = "none";
-                resultsBox.style.animation = ""; // Reset animation property
-            }, 300); // Adjust the timeout to match the animation duration
+                // resultsBox.style.display = "none";
+                ResultsBox.style.animation = ""; // Reset animation property
+            }, 300);
         }
-    }
-};
+    });
+
+    // Handle click on user-typed word to search on Google
+    resultsBox.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+            const query = event.target.textContent.trim();
+            if (query !== "") {
+                const searchUrl = "https://www.google.com/search?q=" + encodeURIComponent(query);
+                window.open(searchUrl, "_blank");
+            }
+        }
+    });
+
+    // Handle input changes to dynamically update the results box
+    inputBox.addEventListener("input", function () {
+        const filter = inputBox.value.trim().toLowerCase();
+        // resultsBox.innerHTML = ""; // Clear the current list
+
+        if (filter !== "") {
+            const newLi = document.createElement("li");
+            const newA = document.createElement("a");
+            newA.href = "#";
+            newA.textContent = filter;
+            newLi.appendChild(newA);
+            resultsBox.appendChild(newLi);
+            ResultsBox.style.display = "block";
+            ResultsBox.style.animation = "to-bottom 0.3s ease-in";
+        } else {
+            ResultsBox.style.animation = "to-top 0.3s ease-in";
+            setTimeout(() => {
+                ResultsBox.style.display = "none";
+                ResultsBox.style.animation = ""; // Reset animation property
+            }, 300);
+        }
+    });
+});
 
 
 
