eedf7079a6837a04fff2e6c0901e9d7368ffa2dc
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2024-09-27 17:37:23 +0530

    optimized auto fill, added visitor count

--- copy.js
+++ copy.js
@@ -321,3 +321,21 @@ document.addEventListener('click', function (event) {
     circularMenu.classList.remove('active');
   }
 });
+
+
+// Disable developer mode
+
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+
+
+document.onkeydown = function(e) {
+  if (e.keyCode == 123 || // F12
+      (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || // Ctrl+Shift+I
+      (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || // Ctrl+Shift+J
+      (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))) { // Ctrl+U
+      e.preventDefault();
+      // window.location.href = 'https://your-redirect-page.com'; // Redirect to your specific page
+  }
+};
